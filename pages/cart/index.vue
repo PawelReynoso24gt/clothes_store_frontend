990d55c6e657c4747009005a725fb7b6ff1dde6d
PawelReynoso24gt <pawelreynoso@gmail.com>
2024-11-03 23:04:00 -0600

    cambio de posicion de pago

--- pages/cart/index.vue
+++ pages/cart/index.vue
@@ -29,7 +29,7 @@
                   <thead>
                     <tr>
                       <th class="product_remove">Remove</th>
-                      <th class="product_thumb">Image</th>
+                      <th class="product_thumb">Hola</th>
                       <th class="product_name">Product</th>
                       <th class="product-price">Price</th>
                       <th class="product_quantity">Quantity</th>
@@ -88,7 +88,7 @@
               </div>
             </div>
           </div>
-          <div class="col-lg-6 col-md-6">
+          <!-- <div class="col-lg-6 col-md-6">
             <div
               class="coupon_code left"
               data-aos="fade-up"
@@ -106,7 +106,7 @@
                 </button>
               </div>
             </div>
-          </div>
+          </div> -->
           <div class="col-lg-6 col-md-6">
             <div
               class="coupon_code right"
@@ -129,24 +129,76 @@
                   <p>Total</p>
                   <p class="cart_amount">${{ cartTotal + 25 }}</p>
                 </div>
-                <div class="checkout_btn">
+                <!-- <div class="checkout_btn">
                   <nuxt-link
                     to="/my-account/checkout-1"
                     class="theme-btn-one btn-black-overlay btn_sm"
                     >Proceed to Checkout</nuxt-link
                   >
-                </div>
+                </div> -->
               </div>
             </div>
           </div>
         </div>
       </div>
     </section>
+    <section class="payment" style="font-size: 20px;">
+        <div class="order_review bg-white">
+            <div class="check-heading">
+                <h2>Forma de pago</h2>
+            </div>
+            <div class="payment_method">
+                <div class="payment_option">
+                    <p style="font-size: 20px;"><b>Ingrese y llene los campos que serán usados para el pago</b></p>
+                    <br>
+                    <div class="form-group-pago">
+                        <label for="transferencia">Pago con Transferencia Bancaria</label>
+                        <input type="number" v-model="pago.transferencia" id="transferencia" class="form-control" placeholder="Monto de transferencia bancaria" />
+                    </div>
+                    <div class="form-group-pago">
+                        <label for="banco">Banco de Transferencia</label>
+                        <input type="text" v-model="pago.banco" id="banco" class="form-control" placeholder="Nombre del banco" />
+                    </div>
+                    <div class="form-group-pago">
+                        <label for="correlativo">Correlativo de Transferencia</label>
+                        <input type="text" v-model="pago.correlativo" id="correlativo" class="form-control" placeholder="Número de correlativo" />
+                    </div>
+                    <div class="form-group-pago">
+                        <label for="tarjetaCredito">Pago con Tarjeta de Crédito</label>
+                        <input type="number" v-model="pago.tarjetaCredito" id="tarjetaCredito" class="form-control" placeholder="Monto de pago con tarjeta de crédito" />
+                    </div>
+                    <div class="form-group-pago">
+                        <label for="numeroCredito">Número de Tarjeta de Crédito</label>
+                        <input type="text" v-model="pago.numeroCredito" id="numeroCredito" class="form-control" placeholder="Número de tarjeta de crédito" />
+                    </div>
+                    <div class="form-group-pago">
+                        <label for="tarjetaDebito">Pago con Tarjeta de Débito</label>
+                        <input type="number" v-model="pago.tarjetaDebito" id="tarjetaDebito" class="form-control" placeholder="Monto de pago con tarjeta de débito" />
+                    </div>
+                    <div class="form-group-pago">
+                        <label for="numeroDebito">Número de Tarjeta de Débito</label>
+                        <input type="text" v-model="pago.numeroDebito" id="numeroDebito" class="form-control" placeholder="Número de tarjeta de débito" />
+                    </div>
+                    <div class="form-group-pago">
+                        <label for="efectivo">Pago con Efectivo</label>
+                        <input type="number" v-model="pago.efectivo" id="efectivo" class="form-control" placeholder="Monto en efectivo" />
+                    </div>
+                    <div class="form-group-pago">
+                        <label for="paypal">PayPal</label>
+                        <input type="number" v-model="pago.paypal" id="paypal" class="form-control" placeholder="Monto de pago con PayPal" />
+                    </div>
+                </div>
+            </div>
+            <!-- <button class="theme-btn-one btn-black-overlay btn_sm">Place Order</button> -->
+            <button @click="hacerCompra" class="buttonPay">Completar Orden</button>
+        </div>
+    </section>
   </div>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
+import axios from "axios";
 
 export default {
   name: "cart",
@@ -154,7 +206,6 @@ export default {
   data() {
     return {
       title: "Cart",
-      // Breadcrumb Items Data
       breadcrumbItems: [
         {
           text: "Home",
@@ -165,8 +216,17 @@ export default {
           to: "/cart",
         },
       ],
-
-      // Product Quanity Increment/ Decrement Data
+      pago: {
+        transferencia: 0.0,
+        banco: "",
+        correlativo: "",
+        tarjetaCredito: 0.0,
+        numeroCredito: "",
+        tarjetaDebito: 0.0,
+        numeroDebito: "",
+        efectivo: 0.0,
+        paypal: 0.0,
+      },
       quantity: 1,
     };
   },
@@ -177,26 +237,49 @@ export default {
       cartTotal: "cart/cartTotalAmount",
     }),
   },
+
   mounted() {
-    // For scroll page top for every Route
     window.scrollTo(0, 0);
   },
+
   methods: {
     getImageUrl(path) {
       return require("@/assets/img/product-image/" + path);
     },
-    // Discount Price
+
     discountedPrice(product) {
       const price = product.price - (product.price * product.discount) / 100;
       return price;
     },
-    // For Delete/Remove Product Item
+
     removeCartItem: function (product) {
       this.$store.dispatch("cart/removeCartItem", product);
     },
+
+    async hacerCompra() {
+      try {
+        const response = await axios.post("http://localhost:5000/pagos/create", this.pago);
+        alert("Compra realizada exitosamente!");
+        console.log("Respuesta del servidor:", response.data);
+        
+        this.pago = {
+          transferencia: 0.0,
+          banco: "",
+          correlativo: "",
+          tarjetaCredito: 0.0,
+          numeroCredito: "",
+          tarjetaDebito: 0.0,
+          numeroDebito: "",
+          efectivo: 0.0,
+          paypal: 0.0,
+        };
+      } catch (error) {
+        console.error("Error al realizar la compra:", error);
+        alert("Hubo un error al procesar el pago.");
+      }
+    },
   },
 
-  // Page head() Title, description for SEO
   head() {
     return {
       title: this.title,
@@ -211,3 +294,21 @@ export default {
   },
 };
 </script>
+// CSS 
+<style scoped>
+    .form-group-pago {
+        margin-right: 1000px;
+        margin-bottom: 20px;
+    }
+
+    .form-group-pago input{
+        font-size: 20px;
+    }
+
+    .buttonPay {
+        background-color: #4CAF50;
+        color: #fff;
+        padding: 10px 20px;
+        border: none;
+    }
+</style>
